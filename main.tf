5d59f69384f2047c48091b1c4eee1e8a408744be
RemixTro <74962232+RemixTro@users.noreply.github.com>
2026-05-27 19:04:36 +0200

    GitHub Automatization1

--- main.tf
+++ main.tf
@@ -19,6 +19,12 @@ resource "azurerm_virtual_network" "vnet" {
     name             = "subnet1"
     address_prefixes = ["10.0.1.0/24"]
   }
+
+    subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+  }
+
 }
 
 
