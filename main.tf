5940e2db5646946ac403d41be1efdfbbc69d1c29
RemixTro <74962232+RemixTro@users.noreply.github.com>
2026-05-30 18:56:12 +0200

    Load Balancer, infrastructure configuration 2

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "azurerm_storage_container" "sc" {
 
 resource "azurerm_network_interface" "nic" {
   count               = 2
-  name                = "devops-nic"
+  name                = "devops-nic-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -136,12 +136,12 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_linux_virtual_machine" "example" {
   count               = 2
-  name                = "vm-devops-project"
+  name                = "vm-devops-project-${count.index}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2ats_V2"
   admin_username      = "adminuser"
-  disable_password_authentication = "false"
+  disable_password_authentication = false
   admin_password = "Kwasnejablko1234!"
   
 
@@ -159,7 +159,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
 
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.nic[count.index].id,
   ]
     
   os_disk {
@@ -186,18 +186,18 @@ resource "azurerm_lb" "lbi" {
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.pip.id
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "lbib" {
   loadbalancer_id = azurerm_lb.lbi.id
   name            = "BackEndAddressPool"
-  virtual_network_id = azurerm_virtual_network.vnet
+  virtual_network_id = azurerm_virtual_network.vnet.id
 }
 
 resource "azurerm_lb_rule" "lb_rule" {
-  loadbalancer_id                = azurerm_lb.web_lb.id
+  loadbalancer_id                = azurerm_lb.lbi.id
   name                           = "HTTPRule"
   protocol                       = "Tcp"
   frontend_port                  = 80
@@ -209,7 +209,7 @@ resource "azurerm_lb_rule" "lb_rule" {
 
 resource "azurerm_network_interface_backend_address_pool_association" "nicLB" {
   count                   = 2
-  network_interface_id    = azurerm_network_interface.vm_nic[count.index].id
+  network_interface_id    = azurerm_network_interface.nic[count.index].id
   ip_configuration_name   = "internal"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend.id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.lbib.id
 }
