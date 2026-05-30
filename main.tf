9ed47b44fb7f963bd1ff905c3e8626e57b29487c
RemixTro <74962232+RemixTro@users.noreply.github.com>
2026-05-30 19:13:26 +0200

    Load Balancer, infrastructure configuration 4

--- main.tf
+++ main.tf
@@ -193,7 +193,7 @@ resource "azurerm_lb" "lbi" {
 resource "azurerm_lb_backend_address_pool" "lbib" {
   loadbalancer_id = azurerm_lb.lbi.id
   name            = "BackEndAddressPool"
-  virtual_network_id = azurerm_virtual_network.vnet.id
+ 
 }
 
 resource "azurerm_lb_rule" "lb_rule" {
