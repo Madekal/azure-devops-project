583742cde2cd93928113c60de486e21240d2af17
RemixTro <74962232+RemixTro@users.noreply.github.com>
2026-06-01 00:06:25 +0200

    Health Probe - fix

--- main.tf
+++ main.tf
@@ -204,6 +204,7 @@ resource "azurerm_lb_rule" "lb_rule" {
   backend_port                   = 80
   frontend_ip_configuration_name = "PublicIPAddress"
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbib.id]
+  probe_id                       = azurerm_lb_probe.lb_probe.id
 }
 
 
@@ -225,14 +226,3 @@ resource "azurerm_lb_probe" "lbp" {
 
 }
 
-
-resource "azurerm_lb_rule" "lb_rule" {
-  loadbalancer_id                = azurerm_lb.lbi.id
-  name                           = "HTTPRule"
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "PublicIPAddress"
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lbib.id]
-  probe_id                       = azurerm_lb_probe.lb_probe.id # <-- Dopisana relacja do sondy!
-}
