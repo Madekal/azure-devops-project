b6cf8f2c142f273da941d07485aacfcecc3666cb
RemixTro <74962232+RemixTro@users.noreply.github.com>
2026-06-01 00:22:47 +0200

    Variables + Sub ID hide

--- main.tf
+++ main.tf
@@ -140,7 +140,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2ats_V2"
-  admin_username      = "adminuser"
+  admin_username      = var.vm_admin_password
   disable_password_authentication = false
   admin_password = "Kwasnejablko1234!"
   
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "vm_admin_password" {
+  type        = string
+  description = "Haslo administratora do maszyn wirtualnych"
+  sensitive   = true
+}
